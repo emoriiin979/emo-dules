935a46fdc1b994d42f7eda06ff0e53a76c7eb507
emoriiin979 <emoriiin979@gmail.com>
2026-02-16 21:15:58 +0900

    sftp系関数を公開

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { createDbClient, transaction } from './modules/db/index.js';
 export { httpFetch } from './modules/http.js';
 export { createLogFuncs, createLoggerMiddleware } from './modules/log.js';
 export { parseCsv, parseCustomFormat } from './modules/parse.js';
+export { createSftpFuncs } from './modules/sftp.js';
 export { createFileOperators } from './modules/storage/index.js';
 export { mergeUrl, addUrlParams } from './modules/url.js';
