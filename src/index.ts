a4fd1d104df9b4d23a9f7c9c13b41baaa5a48ee9
emoriiin979 <emoriiin979@gmail.com>
2026-02-16 15:02:00 +0900

    log系関数公開

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { createDbClient, transaction } from './modules/db/index.js';
 export { httpFetch } from './modules/http.js';
+export { createLogFuncs, createLoggerMiddleware } from './modules/log.js';
 export { parseCsv, parseCustomFormat } from './modules/parse.js';
 export { createFileOperators } from './modules/storage/index.js';
 export { mergeUrl, addUrlParams } from './modules/url.js';
